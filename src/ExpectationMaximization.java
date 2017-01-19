fe43f2027b3bb9679d289465afdee148a93647f7
Albert Harounian <albert.harounian@gmail.com>
2017-01-19 19:59:11 +0200

    Added EX processing class

---
+++ src/ExpectationMaximization.java
@@ -0,0 +1,9 @@
+public class ExpectationMaximization {
+
+
+    public void init(DevelopmentSet developmentSet){
+        // Todo:
+        // 1. Split to 9 clusters in a modulo-9 manner
+        // 2. Compute Ai, Pik
+    }
+}
