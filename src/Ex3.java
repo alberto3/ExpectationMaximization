5f0d17f28ce3fe838d094198fc3bb9a6d295b844
Albert Harounian <albert.harounian@gmail.com>
2017-01-20 14:39:56 +0200

    Added clusters init

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,12 +1,17 @@
 public class Ex3 {
 
     public static void main(String[] args) {
+        final int maxClusters = 9;
+
         // Load input file
         ProcessInputData processFileData = new ProcessInputData("dataset/develop.txt");
 
         // Prepare the development set
         DevelopmentSet developmentSet = processFileData.readInputFile();
 
+        ExpectationMaximization expectationMaximization = new ExpectationMaximization();
 
+        // Init the EM algorithm
+        expectationMaximization.init(developmentSet, maxClusters);
     }
 }
--- src/ExpectationMaximization.java
+++ src/ExpectationMaximization.java
@@ -1,9 +1,39 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
 public class ExpectationMaximization {
+    private Map<Integer, List<Article>> clusters;
+    private DevelopmentSet developmentSet;
+    private int maxClusters;
 
 
-    public void init(DevelopmentSet developmentSet){
+    public void init(DevelopmentSet developmentSet, int maxClusters) {
+        this.developmentSet = developmentSet;
+        this.maxClusters = maxClusters;
+
+        initClusters();
         // Todo:
-        // 1. Split to 9 clusters in a modulo-9 manner
         // 2. Compute Ai, Pik
+    }
+
+    private void initClusters() {
+        clusters = new HashMap<>();
+
+        developmentSet.getArticles().forEach(new Consumer<Article>() {
+            private int index = 0;
+
+            @Override
+            public void accept(Article article) {
+                int key = index % maxClusters;
+
+                if (!clusters.containsKey(key)) {
+                    clusters.put(key, new ArrayList<>());
+                }
+                clusters.get(key).add(article);
+            }
+        });
     }
 }
