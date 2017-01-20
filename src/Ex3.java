79c0f1baa95be6cd4031f65ad8af7fe19bff14e9
Albert Harounian <albert.harounian@gmail.com>
2017-01-20 14:46:21 +0200

    Merged with master

--- src/Ex3.java
+++ src/Ex3.java
@@ -10,14 +10,8 @@ public class Ex3 {
         DevelopmentSet developmentSet = processFileData.readInputFile();
 
         ExpectationMaximization expectationMaximization = new ExpectationMaximization();
-<<<<<<< HEAD
 
         // Init the EM algorithm
         expectationMaximization.init(developmentSet, maxClusters);
-=======
-      
-        // Init the EM algorithm
-        expectationMaximization.init(DS);
->>>>>>> c14fd4be093096b6a86427ad3dd71eb51346777c
     }
 }
